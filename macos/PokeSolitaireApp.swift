9cdf1b26837f1a9bbd65419b388900b490e7b397
toma itagaki <45250757+tomaitagaki@users.noreply.github.com>
2026-04-01 21:10:03 -0400

    Add the macOS app entry point

---
+++ macos/PokeSolitaireApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct PokeSolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.automatic)
+    }
+}
